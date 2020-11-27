ea6ee9c9009efbf6ca39de69bfa10ccfda937386
Your Name <you@example.com>
2020-11-27 02:29:42 +0000

    adding of some code into the backend and front end

---
+++ nodeBackend/public/scripts.js
@@ -0,0 +1,26 @@
+//button variable
+const geoButton = document.querySelector('.geo-button');
+geoButton.addEventListener('click', getLocation);
+//check for geolocation availability
+//harvist that data
+
+function getLocation(){
+
+if("geolocation" in navigator){
+    console.log("geolocation is available");
+
+    navigator.geolocation.getCurrentPosition(position =>{
+        const lat = position.coords.latitude; 
+        const long = position.coords.longitude;
+
+        document.getElementById('latitude').textContent = lat;
+        document.getElementById('longitude').textContent =long;
+    });
+}
+
+
+ else {
+    console.log("geolocation is not available");
+}
+
+}
